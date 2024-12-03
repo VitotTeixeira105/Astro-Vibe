b2cb8f52d7e17269ba4a66769c3074615ccd4856
Vitor Teixeira <134493737+VitotTeixeira105@users.noreply.github.com>
2024-12-03 10:47:40 +0000

    Add files via upload

---
+++ aries.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <!--FONTES DE LETRAS EXTERNAS - GOOGLE FONTS-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Agdasima:wght@400;700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Agdasima:wght@400;700&family=Cinzel:wght@400..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Agdasima:wght@400;700&family=Cinzel:wght@400..900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <!--FONTES DE SIMBOLOS EXTERNAS - PICTOGRAMMERS-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@7.4.47/css/materialdesignicons.min.css">
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="estilosPaginasSignos.css">
+    <title>Astro Vibe</title>
+</head>
+<style>
+    .wallpaperInicio {
+        background-image: url(ariesFundo.jpg);
+    }
+</style>
+
+<body>
+    <div id="div-header"></div>
+
+    <div class="wallpaperInicio"></div>
+
+    <div class="div-title-site">
+        <h1>Aries</h1>
+    </div>
+
+    <div class="signoDescricaoFer">
+        <div class="image-container">
+            <img src="ariesLogo.webp" alt="Aries Logo">
+        </div>
+        <div class="text-container">
+        <p>Áries é o primeiro signo do zodíaco, representado pelo elemento Fogo e regido por Marte. Este signo simboliza energia, liderança e coragem, sendo conhecido por sua natureza independente e impulsiva. Arianos tendem a enfrentar desafios de frente, movidos por um entusiasmo contagiante e uma determinação inabalável.</p>
+<p>Com uma personalidade apaixonada, os nativos de Áries são pioneiros natos, sempre buscando novos começos e aventuras. São motivados pelo desejo de vencer, mas precisam aprender a controlar sua impaciência e a lidar com frustrações de forma equilibrada.</p>
+<p>Embora possam ser vistos como competitivos e diretos, os arianos possuem um lado generoso e protetor. Sua força interior e capacidade de inspirar os outros os tornam amigos leais e parceiros apaixonados, prontos para conquistar o mundo ao lado daqueles que amam.</p>
+
+        </div>
+    </div>
+
+    <div id="rodape"></div>
+</body>
+<script src="script.js"></script>
+<script src="scriptRodape.js"></script>
+<script src="scriptHeader.js"></script>
+
+</html>
